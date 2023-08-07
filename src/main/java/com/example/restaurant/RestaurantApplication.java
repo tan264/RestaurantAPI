930f166ac72d0e090fe46af0ad72a9b376e647b4
tan264 <tandang264@gmail.com>
2023-08-07 20:45:44 +0700

    Config swagger to fit with HTTPS

--- src/main/java/com/example/restaurant/RestaurantApplication.java
+++ src/main/java/com/example/restaurant/RestaurantApplication.java
@@ -2,11 +2,12 @@ package com.example.restaurant;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "API VJP PRO", version = "1.0.0", description = "Project đầu tiên với Java mùa xuân khởi động"))
+@OpenAPIDefinition(info = @Info(title = "API VJP PRO", version = "1.0.0", description = "Project Java mùa xuân khởi động"), servers = {@Server(url = "/api", description = "Default Server URL")})
 public class RestaurantApplication {
 
     public static void main(String[] args) {
